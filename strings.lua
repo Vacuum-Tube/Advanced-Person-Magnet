4d71868ed6008e4d1f55d1398713aa6c3be31bcc
VacuumTube <80115559+Vacuum-Tube@users.noreply.github.com>
2024-06-08 15:52:06 +0200

    steam description

--- strings.lua
+++ strings.lua
@@ -2,43 +2,47 @@ function data()
 return {
 	en = {
 		["mod_desc"] = [[
-Advanced Attraction Point/Magnet for Persons with several adjustable settings:
--Model
--Capacity
--Street type (including Mods)
--Street length
--Position movement
+Attraction Point/Magnet for Persons with several adjustable settings:
+[list]
+[*]Model
+[*]Capacity
+[*]Street type (including Mods)
+[*]Street length
+[*]Position movement
+[/list]
 
-The special thing about this mod is the selection of ALL streets in the game. Thus, everyone can adjust the street type and width by using loaded street mods. With a large number of streets, this can quickly become confusing. Unfortunately, the streets are only given sorted alphabetically according to the file name (not to the loading order). To bring some order in, only 2-lane streets are shown and country streets are sorted lastly.
-		]],
+The special thing about this mod is the selection of [b]ALL[/b] streets in the game. Thus, everyone can adjust the street type and width by using loaded street mods. With a large number of streets, this can quickly become confusing. Unfortunately, the streets are only given sorted alphabetically according to the file name (not to the loading order). To bring some order in, only 2-lane streets are shown and country streets are sorted lastly.
+]],
 		["apm_con_desc"] = [[
 Adjust the person magnet with the parameters.
 
-Without street: only pedestrians
-With street: pedestrians and cars
+Without street: Only pedestrians !!!
+With street: Pedestrians and cars
 
 Invisible model (APM/invisible.mdl) only recommended with Street! (otherwise hard to find)
-		]],
+]],
 	},
 	de = {
 		["mod_desc"] = [[
-Erweiterter Personen/Besucher-Magnet mit zahlreichen Einstellungsmöglichkeiten:
--Modell
--Kapazität
--Straßentyp (inklusive Mods)
--Straßenlänge
--Positionsverschiebung
+Personen/Besucher-Magnet mit zahlreichen Einstellungsmöglichkeiten:
+[list]
+[*]Modell
+[*]Kapazität
+[*]Straßentyp (inklusive Mods)
+[*]Straßenlänge
+[*]Positionsverschiebung
+[/list]
 
-Das besondere an dieser Mod ist wohl die Auswahl von ALLEN im Spiel vorhandenen Straßen. Somit kann jeder sich Straßentyp und Breite mithilfe von geladenen Straßenmods anpassen. Bei sehr vielen Straßen kann das allerdings schnell unübersichtlich werden. Leider kriegt man die Straßen nur alphabetisch nach dem Dateinamen sortiert (und nicht nach Ladereihenfolge). Um etwas Ordnung reinzubringen, werden nur 2-spurige Straßen angezeigt und Landstraßen werden nach hinten sortiert.
-		]],
+Das besondere an dieser Mod ist wohl die Auswahl von [b]ALLEN[/b] im Spiel vorhandenen Straßen. Somit kann jeder sich Straßentyp und Breite mithilfe von geladenen Straßenmods anpassen. Bei sehr vielen Straßen kann das allerdings schnell unübersichtlich werden. Leider kriegt man die Straßen nur alphabetisch nach dem Dateinamen sortiert (und nicht nach Ladereihenfolge). Um etwas Ordnung reinzubringen, werden nur 2-spurige Straßen angezeigt und Landstraßen werden nach hinten sortiert.
+]],
 		["apm_con_desc"] = [[
 Pass dir den Personen-Magnet mit den Parametern an.
 
-Ohne Straße: Nur Fußgänger
+Ohne Straße: Nur Fußgänger !!!
 Mit Straße: Fußgänger und Autofahrer
 
 Unsichtbares Modell (APM/invisible.mdl) nur mit Straße empfohlen! (Sonst schwer wiederzufinden)
-		]],
+]],
 		["Landuse"] = "Landnutzung",
 		["Person Capacity"] = "Personen Kapazität",
 		["Length"] = "Länge",
